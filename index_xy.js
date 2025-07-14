d6d0f4d4191355acde0bbdff8b51f24946497d22
Hal <xiaobohao@hotmail.com>
2025-07-14 18:38:00 +0800

    在散点图里增加了过滤功能。

--- index_xy.js
+++ index_xy.js
@@ -4,6 +4,90 @@ function saveSul() {
     saveData('suls2d', arSuls);
 }
 
+function updateFilterOptions() {
+    var filterField = $('#selFilterField').val();
+    var container = $('#filterOptionsContainer');
+    container.empty();
+
+    console.log('updateFilterOptions called with field:', filterField);
+
+    if (!filterField || filterField === '') {
+        $('#filterOptionsRow').hide();
+        return;
+    }
+
+    // 获取该字段的所有唯一值
+    var uniqueValues = [];
+    objs.forEach(function (obj) {
+        var value = String(obj[filterField]);
+        if (uniqueValues.indexOf(value) === -1) {
+            uniqueValues.push(value);
+        }
+    });
+
+    // 排序
+    uniqueValues.sort();
+
+    console.log('Found unique values:', uniqueValues);
+
+    // 生成复选框 - 横排布局，自动换行
+    uniqueValues.forEach(function (value) {
+        var safeId = 'filter_' + value.replace(/[^a-zA-Z0-9]/g, '_');
+        var checkbox = $('<div class="form-check form-check-inline">');
+        var input = $('<input class="form-check-input" type="checkbox" value="' + value + '" id="' + safeId + '" checked>');
+        var label = $('<label class="form-check-label" for="' + safeId + '">').text(value);
+
+        checkbox.append(input).append(label);
+        container.append(checkbox);
+    });
+
+    console.log('Generated checkboxes for', uniqueValues.length, 'values');
+    $('#filterOptionsRow').show();
+    updateFilterSummary();
+    ApplyChart(true); // 强制更新图表，清除旧数据点
+}
+
+function selectAllFilterOptions() {
+    $('#filterOptionsContainer input[type="checkbox"]').prop('checked', true);
+    updateFilterSummary();
+    ApplyChart(true); // 强制更新图表
+}
+
+function invertFilterSelection() {
+    $('#filterOptionsContainer input[type="checkbox"]').each(function () {
+        $(this).prop('checked', !$(this).prop('checked'));
+    });
+    updateFilterSummary();
+    ApplyChart(true); // 强制更新图表
+}
+
+function updateFilterSummary() {
+    var filterField = $('#selFilterField').val();
+    var summary = $('#filterSummary');
+
+    if (!filterField || filterField === '') {
+        summary.text('未选择过滤字段');
+        return;
+    }
+
+    var checkedValues = [];
+    $('#filterOptionsContainer input[type="checkbox"]:checked').each(function () {
+        checkedValues.push($(this).val());
+    });
+
+    var totalValues = $('#filterOptionsContainer input[type="checkbox"]').length;
+
+    if (checkedValues.length === 0) {
+        summary.text('已选择: 无');
+    } else if (checkedValues.length === totalValues) {
+        summary.text('已选择: 全部 (' + totalValues + '项)');
+    } else if (checkedValues.length <= 3) {
+        summary.text('已选择: ' + checkedValues.join(', '));
+    } else {
+        summary.text('已选择: ' + checkedValues.slice(0, 3).join(', ') + '... 等' + checkedValues.length + '项');
+    }
+}
+
 function loadSul() {
     arSuls = retrieveData('suls2d');
     if (!arSuls) arSuls = [];
@@ -33,6 +117,7 @@ function run() {
                 $("#selXProp option[value!='']").remove();
                 $("#selYProp option[value!='']").remove();
                 $("#selSizeProp option[value!='']").remove();
+                $("#selFilterField option[value!='']").remove();
 
                 Object.keys(objs[0]).sort().forEach(function (prop) {
                     selGrpProp.append($('<option>', {
@@ -48,6 +133,10 @@ function run() {
                         text: prop
                     }));
                     selSizeProp.append($('<option>', {
+                        value: prop,
+                        text: prop
+                    }));
+                    $('#selFilterField').append($('<option>', {
                         value: prop,
                         text: prop
                     }));
@@ -70,7 +159,34 @@ function ApplyChart(forceRefresh = false) {
     var bXCat = $('#chkXCat').is(':checked');
     var bYCat = $('#chkYCat').is(':checked');
 
-    var result = ConverObjs2XYDataSets(objs,
+    // 获取自定义刻度值
+    var xMin = $('#txtXMin').val();
+    var xMax = $('#txtXMax').val();
+    var yMin = $('#txtYMin').val();
+    var yMax = $('#txtYMax').val();
+
+    // 获取过滤设置
+    var filterField = $('#selFilterField').val();
+    var filteredObjs = objs; // 默认使用所有数据
+
+    if (filterField && filterField !== '') {
+        // 获取选中的过滤值
+        var selectedValues = [];
+        $('#filterOptionsContainer input[type="checkbox"]:checked').each(function () {
+            selectedValues.push($(this).val());
+        });
+
+        // 如果选择了过滤值，则过滤数据
+        filteredObjs = objs.filter(function (obj) {
+            return selectedValues.includes(String(obj[filterField]));
+        });
+
+    }
+
+    console.log('自定义刻度值:', { xMin, xMax, yMin, yMax, autoScale });
+    console.log('过滤设置:', { filterField, selectedValues: selectedValues || [], filteredCount: filteredObjs.length, totalCount: objs.length });
+
+    var result = ConverObjs2XYDataSets(filteredObjs,
         sGrpProp, sXProp, sYProp, sSizeProp);
     console.log(result);
 
@@ -78,7 +194,7 @@ function ApplyChart(forceRefresh = false) {
     var divtab = $("#sumdiv");
     divtab.empty();
     if (sGrpProp && sGrpProp != "") {
-        var statobjs = statObjects(objs, sGrpProp);
+        var statobjs = statObjects(filteredObjs, sGrpProp);
 
         var sTab = CreateHtmlTable(statobjs, false);
         var divtab = $("#sumdiv");
@@ -106,7 +222,7 @@ function ApplyChart(forceRefresh = false) {
     // 取得最大的size值
     var maxSize = 10;
     if (sSizeProp) {
-        maxSize = findMaxObj(objs, sSizeProp)[sSizeProp];
+        maxSize = findMaxObj(filteredObjs, sSizeProp)[sSizeProp];
     }
 
     option = {
@@ -173,8 +289,10 @@ function ApplyChart(forceRefresh = false) {
             {
                 name: sXProp,
                 type: bXCat ? 'category' : 'value',
-                data: bXCat ? (extractAndSortUniqueProperty(objs, sXProp)) : undefined,
+                data: bXCat ? (extractAndSortUniqueProperty(filteredObjs, sXProp)) : undefined,
                 scale: autoScale,
+                min: (!autoScale && xMin !== '') ? parseFloat(xMin) : undefined,
+                max: (!autoScale && xMax !== '') ? parseFloat(xMax) : undefined,
                 interval: 1,
                 axisLabel: {
                     formatter: '{value}',
@@ -196,8 +314,10 @@ function ApplyChart(forceRefresh = false) {
             {
                 name: sYProp,
                 type: bYCat ? 'category' : 'value',
-                data: bYCat ? (extractAndSortUniqueProperty(objs, sYProp)) : undefined,
+                data: bYCat ? (extractAndSortUniqueProperty(filteredObjs, sYProp)) : undefined,
                 scale: autoScale,
+                min: (!autoScale && yMin !== '') ? parseFloat(yMin) : undefined,
+                max: (!autoScale && yMax !== '') ? parseFloat(yMax) : undefined,
                 interval: 1,
                 axisLabel: {
                     formatter: '{value}',
@@ -217,6 +337,18 @@ function ApplyChart(forceRefresh = false) {
         ],
 
     };
+
+    // 调试信息
+    console.log('X轴配置:', {
+        min: (!autoScale && xMin !== '') ? parseFloat(xMin) : undefined,
+        max: (!autoScale && xMax !== '') ? parseFloat(xMax) : undefined,
+        autoScale: autoScale
+    });
+    console.log('Y轴配置:', {
+        min: (!autoScale && yMin !== '') ? parseFloat(yMin) : undefined,
+        max: (!autoScale && yMax !== '') ? parseFloat(yMax) : undefined,
+        autoScale: autoScale
+    });
 
     option.series = result.map(function (r) {
         //console.log(r);
